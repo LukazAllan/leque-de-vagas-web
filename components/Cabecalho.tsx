78fce151f60480facb84a441106b9132d752cbfa
LukazAllan <allanlucas130@gmail.com>
2026-09-13 16:38:18 -0300

    Refactored Cabecalho

--- components/Cabecalho.tsx
+++ components/Cabecalho.tsx
@@ -5,6 +5,7 @@ export default function Cabecalho(){
         <header className="cabecalho">
             <h1>Leque de Vagas</h1>
             <nav>
+                <Link href="/">Home</Link>
                 <Link href="/vagas">Vagas</Link>
                 <Link href="/sobre">Sobre</Link>
             </nav>
