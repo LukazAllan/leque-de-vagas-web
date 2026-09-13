1fdef53719dbd5f6c99a4a0e77663cf986aba4b6
LukazAllan <allanlucas130@gmail.com>
2026-09-13 16:38:37 -0300

    Add Sobre page

---
+++ app/sobre/page.tsx
@@ -0,0 +1,10 @@
+export default function Sobre(){
+    return (
+        <ul id="integrantes">
+            <li>Allan Lucas</li>
+            <li>Arthur Lobo</li>
+            <li>Felipe Duarte</li>
+            <li>Kaike Araújo</li>
+        </ul>
+    );
+}
